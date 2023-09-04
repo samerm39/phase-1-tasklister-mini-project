3d7693c2203f62ab5451b5189ddddddb7862d691
Samer Mobarak <samermobarak888@gmail.com>
2023-09-04 13:35:15 -0400

    mini_project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector(`#create-task-form`).addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newTaskDescription = document.querySelector('#new-task-description').value;
+    const selectedColor = document.querySelector('#color-selector').value; // Get the selected color
+    handleToDo(newTaskDescription, selectedColor); // Pass the selected color to the function
+    const createTaskForm = document.querySelector(`#create-task-form`).reset();
+  });
 });
+
+function handleToDo(todo, color) {
+  let p = document.createElement('p');
+  p.style.color = color; // Set the font color based on the selected color
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = ' x';
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
